fc2bba4710c6ca771dbc4b204336ca77b1cc705c
Lewis Wheeler <Web-Slinger@outlook.com>
2021-02-17 10:52:10 +0000

    added category, colour and occasion filtering.

--- products/views.py
+++ products/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
 from django.db.models import Q
-from .models import Product
+from .models import Product, Category, Colour, Occasion
 
 # Create your views here.
 
@@ -11,20 +11,41 @@ def all_products(request):
 
     products = Product.objects.all()
     query = None
+    categories = None
+    colours = None
+    occasions = None
 
     if request.GET:
+        if 'category' in request.GET:
+            categories = request.GET['category'].split(',')
+            products = products.filter(category__name__in=categories)
+            categories = Category.objects.filter(name__in=categories)
+
+        if 'colour' in request.GET:
+            colours = request.GET['colour'].split(',')
+            products = products.filter(colour__name__in=colours)
+            colours = Colour.objects.filter(name__in=colours)
+
+        if 'occasion' in request.GET:
+            occasions = request.GET['occasion'].split(',')
+            products = products.filter(occasion__name__in=occasions)
+            occasions = Occasion.objects.filter(name__in=occasions)
+
         if 'q' in request.GET:
             query = request.GET['q']
             if not query:
                 messages.error(request, "You didn't enter any search criteria!")
                 return redirect(reverse('products'))
-            
+
             queries = Q(name__icontains=query) | Q(description__icontains=query)
             products = products.filter(queries)
 
     context = {
         'products': products,
         'search_term': query,
+        'current_categories': categories,
+        'current_colours': colours,
+        'current_occasions': occasions,
     }
 
     return render(request, 'products/products.html', context)
