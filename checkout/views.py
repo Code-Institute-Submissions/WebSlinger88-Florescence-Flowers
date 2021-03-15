09e0af5b25272d30d259ceb01e24c4d8c3b39544
Lewis Wheeler <Web-Slinger@outlook.com>
2021-03-15 20:50:13 +0000

    added checkout complete views and urls.

--- checkout/views.py
+++ checkout/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect, reverse
+from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
 from django.conf import settings
 
@@ -85,6 +85,28 @@ def checkout(request):
         'order_form': order_form,
         'stripe_public_key': stripe_public_key,
         'client_secret': intent.client_secret,
+    }
+
+    return render(request, template, context)
+
+
+def checkout_complete(request, order_number):
+    """
+    Handle successful checkouts
+    """
+    save_info = request.session.get('save_info')
+    order = get_object_or_404(Order, order_number=order_number)
+    messages.success(request, f'Your order has been successful! \
+        Your order number is {order_number}. A confirmation \
+        email will be sent to {order.email}. Thank you for \
+        choosing our flowers! We are sure you will love them!')
+
+    if 'bag' in request.session:
+        del request.session['bag']
+
+    template = 'checkout/checkout_success.html'
+    context = {
+        'order': order,
     }
 
     return render(request, template, context)
