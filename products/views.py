bbc1d2816458db984d4bf395c5c7e7a4086b2ef3
Lewis Wheeler <Web-Slinger@outlook.com>
2021-03-22 10:02:59 +0000

    added add products views and urls.

--- products/views.py
+++ products/views.py
@@ -2,13 +2,13 @@ from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
 from django.db.models import Q
 from django.db.models.functions import Lower
-from .models import Product, Category, Colour, Occasion
 
-# Create your views here.
+from .models import Product, Category, Colour, Occasion
+from .forms import ProductForm
 
 
 def all_products(request):
-    """ A view to show all products, including sorting and search queries """
+    """ A view showing all flowers, including sorting and search queries """
 
     products = Product.objects.all()
     query = None
@@ -55,10 +55,12 @@ def all_products(request):
         if 'q' in request.GET:
             query = request.GET['q']
             if not query:
-                messages.error(request, "You didn't enter any search criteria!")
+                messages.error(
+                    request, "You didn't enter any search criteria!")
                 return redirect(reverse('products'))
 
-            queries = Q(name__icontains=query) | Q(description__icontains=query)
+            queries = Q(
+                name__icontains=query) | Q(description__icontains=query)
             products = products.filter(queries)
 
     current_sorting = f'{sort}_{direction}'
@@ -76,7 +78,7 @@ def all_products(request):
 
 
 def product_detail(request, product_id):
-    """ A view to show individual product details """
+    """ A view to show individual flowers' details """
 
     product = get_object_or_404(Product, pk=product_id)
 
@@ -85,3 +87,15 @@ def product_detail(request, product_id):
     }
 
     return render(request, 'products/product_detail.html', context)
+
+
+def add_product(request):
+    """ Add new flowers to the website """
+
+    form = ProductForm()
+    template = 'products/add_product.html'
+    context = {
+        'form': form,
+    }
+
+    return render(request, template, context)
