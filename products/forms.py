2b173113ee11b239b29b6a75f0ff4ec0128255f0
Lewis Wheeler <Web-Slinger@outlook.com>
2021-03-27 15:09:03 +0000

    removed style classes.

--- products/forms.py
+++ products/forms.py
@@ -19,5 +19,4 @@ class ProductForm(forms.ModelForm):
 
         self.fields['category'].choices = friendly_names
         for field_name, field in self.fields.items():
-            field.widget.attrs['class'] = 'rounded-corners \
-                 stripe-style-input form-control'
+            field.widget.attrs['class'] = 'rounded-corners'
