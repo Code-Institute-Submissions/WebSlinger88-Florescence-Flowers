836984c8214ba87adaf5d25b424dbce25142bde9
Lewis Wheeler <Web-Slinger@outlook.com>
2021-03-27 19:26:07 +0000

    added product review urls.

--- products/urls.py
+++ products/urls.py
@@ -9,4 +9,9 @@ urlpatterns = [
     path('add/', views.add_product, name='add_product'),
     path('edit/<int:product_id>/', views.edit_product, name='edit_product'),
     path('delete/<int:product_id>/', views.delete_product, name='delete_product'),
+    path('add_review/<int:product_id>/', views.add_review, name='add_review'),
+    path('update_review/<int:review_id>/<int:rating_id>/', views.update_review,
+         name='update_review'),
+    path('delete_review/<int:review_id>/', views.delete_review,
+         name='delete_review'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
