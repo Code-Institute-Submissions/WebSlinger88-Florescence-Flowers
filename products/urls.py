031a2d8137c86da517d79018b7d1392b4c67e7c5
Lewis Wheeler <Web-Slinger@outlook.com>
2021-04-02 09:12:01 +0000

    edited update/delete urls.

--- products/urls.py
+++ products/urls.py
@@ -11,8 +11,8 @@ urlpatterns = [
     path('delete/<int:product_id>/', views.delete_product,
          name='delete_product'),
     path('add_review/<int:product_id>/', views.add_review, name='add_review'),
-    path('update_review/<int:review_id>/<int:rating_id>/', views.update_review,
+    path('update_review/<int:review_id>/<int:rating_id>', views.update_review,
          name='update_review'),
-    path('delete_review/<int:review_id>/', views.delete_review,
+    path('delete_review/<int:review_id>', views.delete_review,
          name='delete_review'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
